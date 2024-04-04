ac6b5f4d7a8a0bdcbc6a239d195049fff0114ba0
EICH <thedimond22@mail.ru>
2024-04-04 12:12:40 +0300

    Add Csharp seminar

---
+++ task1/Program.cs
@@ -0,0 +1,25 @@
+﻿/*
+Напишите программу, которая на вход
+принимает два целых числа и проверяет,
+является ли первое число квадратом второго.
+a = 25, b = 5 => да
+a = 2, b = 10 => нет
+a = 9, b = -3 => да
+a = -3, b = 9 => нет
+*/
+
+System.Console.WriteLine("Введите два числа");
+int num1 = Convert.ToInt32(Console.ReadLine());
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+System.Console.WriteLine(" Первое число " + num1);
+System.Console.WriteLine(" Первое число " + num2);
+
+if(num2 == num1 * num1)
+{    
+    System.Console.WriteLine("yes");
+}
+else
+{
+System.Console.WriteLine("no");
+}
---
+++ task2/Program.cs
@@ -0,0 +1,17 @@
+﻿/*
+Напишите программу, которая на вход принимает
+целое число N, а на выходе показывает все целые
+числа в промежутке от -N до N.
+Примеры
+4 => -4, -3, -2, -1, 0, 1, 2, 3, 4
+2 => -2, -1, 0, 1, 2
+*/
+
+System.Console.WriteLine("Введите число");
+int num1 = Convert.ToInt32(Console.ReadLine());
+
+for (int i = -num1; i <= num1; i++)
+{
+    System.Console.Write(i + " ");
+    Thread.Sleep(50);
+}
